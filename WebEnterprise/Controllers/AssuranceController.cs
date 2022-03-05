fda39ce5473d222ebf691a81b17b212e61835dde
hungpham2020 <81425170+hungpham2020@users.noreply.github.com>
2022-03-05 15:54:14 +0700

    123

---
+++ WebEnterprise/Controllers/AssuranceController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebEnterprise.Data;
+using WebEnterprise.Models;
+using WebEnterprise.Models.DTO;
+
+namespace WebEnterprise.Controllers
+{
+    public class AssuranceController : Controller
+    {
+        private readonly UserManager<CustomUser> userManager;
+        private readonly ApplicationDbContext context;
+
+        public AssuranceController(UserManager<CustomUser> _userManager, ApplicationDbContext _context)
+        {
+            userManager = _userManager;
+            context = _context;
+        }
+        public IActionResult Index()
+        {
+            var assurances = (from u in context.Users
+                         join ur in context.UserRoles on u.Id equals ur.UserId
+                         join r in context.Roles on ur.RoleId equals r.Id
+                         where r.Name == "Assurance"
+                         select new UserDTO
+                         {
+                             Id = u.Id,
+                             //FullName = u.FullName,
+                             UserName = u.UserName,
+                             Email = u.Email,
+                         }).ToList();
+            return View(assurances);
+        }
+
+        public IActionResult AddAssurance()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAssurance(UserDTO user)
+        {
+            if (ModelState.IsValid)
+            {
+                var account = new CustomUser
+                {
+                    UserName = user.UserName,
+                    FullName = user.FullName,
+                    Email = user.Email,
+                };
+                var result = await userManager.CreateAsync(account, "Abc@12345");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(account, "Assurance");
+                    TempData["message"] = $"Successfully Add new Assurance {account.FullName}";
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(user);
+        }
+
+        public IActionResult EditAssurance(string id)
+        {
+            var assurance = context.Users.FirstOrDefault(u => u.Id == id);
+            if (assurance == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(assurance);
+        }
+
+        [HttpPost]
+        public IActionResult EditAssurance(UserDTO assurance)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(assurance).State = EntityState.Modified;
+                context.SaveChanges();
+                TempData["message"] = $"Successfully Edit Assurance {assurance.FullName}";
+                return RedirectToAction("Index");
+            }
+            return View(assurance);
+        }
+
+        public IActionResult DeleteAssurance(string id)
+        {
+            var assurance = context.Users.FirstOrDefault(s => s.Id == id);
+            if (assurance == null)
+            {
+                return RedirectToAction("Index");
+            }
+            context.Remove(assurance);
+            context.SaveChanges();
+            TempData["message"] = $"Successfully Delete Assurance";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebEnterprise/Models/DTO/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace WebEnterprise.Models.DTO
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string? FullName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
