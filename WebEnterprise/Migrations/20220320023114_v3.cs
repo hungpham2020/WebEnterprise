c9cfd0fcd7d93c8f70e7eb81976d0652e5a94711
hungpham2020 <81425170+hungpham2020@users.noreply.github.com>
2022-03-20 09:31:25 +0700

    123

---
+++ WebEnterprise/Migrations/20220320023114_v3.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebEnterprise.Migrations
+{
+    public partial class v3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_CustomUserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_CustomUserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CustomUserId",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorId",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "4b66d0a4-aa39-45c7-811a-fb76c763bbe5", "AQAAAAEAACcQAAAAELc7cXwMP13p21n9t8MO6xZjJAW/+afp9523QIUSaN9ITPlQwEhgSvvOFTMgdW4I8Q==", "e9b656e8-66e3-4e54-9300-b81066818f92" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Comments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomUserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "5092bce4-b08a-42a5-b414-75c3d48e2de1", "AQAAAAEAACcQAAAAEADizNtBctSJLymuJtghvV5UYT0Vt/bA/lj9Rsoh9jfClgNEd9+aU/Id5QmQ3iiMlA==", "20e96615-7055-4d17-b2f1-91ab1af89b57" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_CustomUserId",
+                table: "Comments",
+                column: "CustomUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_CustomUserId",
+                table: "Comments",
+                column: "CustomUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+        }
+    }
+}
