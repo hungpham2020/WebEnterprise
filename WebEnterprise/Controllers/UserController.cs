c708723e2a60d18ed5cd7490e45edfd94b1bd0a1
Hieuln99 <87683551+Hieuln99@users.noreply.github.com>
2022-03-21 20:51:45 +0700

    ok
    nice

--- WebEnterprise/Controllers/UserController.cs
+++ WebEnterprise/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 using WebEnterprise.Data;
 using WebEnterprise.Models;
@@ -18,7 +20,51 @@ namespace WebEnterprise.Controllers
             userManager = _userManager;
             context = _context;
         }
+
+        //-----------------------------------------
+        private void ViewCat()
+        {
+            ViewBag.Cat = context.Categories.ToList();
+        }
+        private void ViewCate()
+        {
+            var cats = context.Categories.Select(c => new Category
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Description = c.Description,
+            }).ToList();
+
+            ViewBag.Cat = new SelectList(cats, "Id", "Name");
+        }
+        private List<Category> LoadCat(string form)
+        {
+            if (form != null)
+            {
+                var selected = form.Split(',').Select(id => Int32.Parse(id)).ToArray();
+                return context.Categories.Where(c => selected.Contains(c.Id)).ToList();
+            }
+            else
+            {
+                return context.Categories.Where(c => c.Id == 0).ToList();
+            }
+        }
+        private void SelectedCat(int id)
+        {
+            var selectedCat = context.Categories.FirstOrDefault(c => c.Id == id);
+            var cats = context.Categories.Select(c => new Category
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Description = c.Description,
+            }).ToList();
+            ViewBag.SelectedCat = new SelectList(cats, selectedCat);
+        }
+
+        //-----------------------------------
+
         public IActionResult Index()
+        
         {
             var posts = (from p in context.Posts
                          join u in context.Users on p.UserId equals u.Id
@@ -31,46 +77,159 @@ namespace WebEnterprise.Controllers
                              CatId = c.Id,
                              CatName = c.Name,
                              AuthorName = u.FullName,
-                         }).ToList();
+                         }).ToList(); 
+            
+            ViewCat();
             return View(posts);
-
         }
 
 
         [HttpPost]
-        //public IActionResult Comment(Comment c, IFormCollection f)
-        //{
-        //    var p = Convert.ToInt32(f["PostId"]);
-        //    Post post = context.Posts.Where(u => u.Id == p).FirstOrDefault();
-        //    CustomUser user = context.Users.Where(u => u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier)).FirstOrDefault();
+        public IActionResult Comment(CommentDTO c)
+        {
+            var comment = new Comment();
+            comment.Description = c.Description;
+            comment.PostId = c.PostId;
+            comment.UpdateTime = DateTime.Now;
+            comment.AuthorId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        //    var comment = new Comment();
-        //    comment.Description = c.Description;
-        //    comment.Post = post;
-        //    comment.UpdateTime = DateTime.Now;
-        //    comment.CustomUser = user;
+            context.Comments.Add(comment);
+            context.SaveChanges();
+            return RedirectToAction("ShowComment",new { id = comment.PostId });
+        }
 
-        //    context.Comments.Add(comment);
-        //    context.SaveChanges();
-        //    return RedirectToAction("ShowComment");
-        //}
 
+        [HttpGet]
         public IActionResult ShowComment(int id)
         {
+            ViewBag.PostID = id;
             var comments = (from p in context.Posts
                             join c in context.Comments on p.Id equals c.PostId
+                            join u in context.Users on c.AuthorId equals u.Id
                             where c.PostId == id
+                            
                             select new CommentDTO
                             {
                                 CommentId = c.Id,
                                 UpdateTime = c.UpdateTime,
                                 Description = c.Description,
+                                AuthorName = u.FullName,
                                 PostDescription = p.Description,
                                 PostId = id,
                             }).ToList();
             return View(comments);
         }
 
+        [HttpPost]
+        public IActionResult EditComment(int id)
+        {
+            return View();
+        }
+
+        public IActionResult DeleteComment(int id)
+        {
+            var comment = context.Comments.FirstOrDefault(c => c.Id == id);
+
+            context.Comments.Remove(comment);
+            context.SaveChanges();
+            TempData["message"] = $"Delete Comment Successfully!";
+            return RedirectToAction("ShowComment", new { id = comment.PostId });
+        }
+
+
+
+        //-----------------------------------------------------
+
+
+        [HttpPost]
+        public IActionResult AddPost(PostDTO res, IFormCollection f)
+        {
+            var cat = LoadCat(f["CatIds"]);
+            foreach (var c in cat)
+            {
+                res.CatId = c.Id;
+            }
+            if (!ModelState.IsValid)
+            {
+                var post = new Post();
+                post.Title = res.Title;
+                post.Description = res.Description;
+                post.CateId = res.CatId;
+                post.OpenDate = DateTime.Now;
+                post.ClosedDate = post.OpenDate.AddDays(14);
+                post.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                context.Posts.Add(post);
+                context.SaveChanges();
+                TempData["message"] = $"Successfully Add new Post {post.Title}";
+                return RedirectToAction("Index");
+            }
+            return Content("Cannot Add");
+
+        }
+        
+        [HttpGet]
+        public IActionResult EditPost(int id)
+        {
+            var post = context.Posts.Where(u => u.Id == id).Select(u => new PostDTO
+            {
+                Id = id,
+                Title = u.Title,
+                Description = u.Description,
+                OpenDate = u.OpenDate,
+                ClosedDate = u.ClosedDate,
+                UserId = u.UserId,
+                CatId = u.CateId
+            }).FirstOrDefault();
+            if (post.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+
+                ViewCate();
+                SelectedCat(id);
+                return View(post);
+            }
+            TempData["message"] = $"You can not edit post {post.Title}, cause you noy owner! ";
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditPost(PostDTO res)
+        {
+            var post = context.Posts.Find(res.Id);
+            if (post != null)
+            {
+                post.Id = res.Id;
+                post.Title = res.Title;
+                post.Description = res.Description;
+                post.OpenDate = res.OpenDate;
+                post.ClosedDate = res.ClosedDate;
+                post.CateId = res.CatId;
+
+                //string extension = Path.GetExtension(res.FileUpload.FileName);
+                //string newname = post.Title + extension;
+                //post.File = await FileControl.UploadFile(res.FileUpload, @"postFiles\", newname.ToLower());
+                context.Entry(post).State = EntityState.Modified;
+                context.SaveChanges();
+                TempData["message"] = $"Successfully Edit Post {post.Title}";
+                return RedirectToAction("Index");
+            }
+            SelectedCat(res.Id);
+            return View(res);
+        }
+
+        public IActionResult DeletePost(int id)
+        {
+            var post = context.Posts.FirstOrDefault(p => p.Id == id);
+            if (post.UserId == User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+                context.Remove(post);
+                context.SaveChanges();
+                TempData["message"] = $"Successfully Delete Post {post.Title}";
+                return RedirectToAction("Index");
+            }
+            TempData["message"] = $"Can not delete post {post.Title}, cause you are not owner!";
+            return RedirectToAction("Index");
+        }
     }
 }
 
