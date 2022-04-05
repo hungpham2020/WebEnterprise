a8206eadfb430b7d44294c53c131e468ad20c6e6
hungpham2020 <81425170+hungpham2020@users.noreply.github.com>
2022-04-05 18:29:23 +0700

    123

---
+++ WebEnterprise/Test/NUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebEnterprise.Data;
+using WebEnterprise.Models;
+
+namespace WebEnterprise.Test
+{
+    [TestClass]
+    public class NUnitTest
+    {
+        private readonly UserManager<CustomUser> userManager;
+        private readonly ApplicationDbContext context;
+
+        public NUnitTest(UserManager<CustomUser> _userManager, ApplicationDbContext _context)
+        {
+            userManager = _userManager;
+            context = _context;
+        }
+
+
+        [TestMethod]
+        public async void TestAddUser()
+        {
+            var result = new IdentityResult();
+
+            using (var trans = context.Database.BeginTransaction())
+            {
+                var account = new CustomUser
+                {
+                    UserName = "test case 1",
+                    FullName = "test case 1",
+                    Email = "testcase1@gmailcom",
+                };
+
+                result = await userManager.CreateAsync(account, "Abc@12345");
+
+                trans.Rollback();
+            }
+
+            Assert.IsTrue(result.Succeeded);
+        }
+    }
+}
